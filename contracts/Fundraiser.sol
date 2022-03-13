11d5eb0f70dba5212745cff5a676e6a0520a7575
YD <ydglitch@gmail.com>
2022-03-13 17:08:18 +0000

    fundraiser-contract

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -2,8 +2,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Fundraiser {
     string public name;
-    constructor (string memory _name) {
+    string public url;
+    string public imageURL;
+    string public bio;
+    
+    constructor(
+        string memory _name,
+        string memory _url,
+        string memory _imageURL,
+        string memory _description
+    ) 
+    public
+    {
         name = _name;
+        url = _url;
+        imageURL = _imageURL;
+        bio = _description;
     }
 
 }
