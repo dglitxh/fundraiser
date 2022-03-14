31a095aceb5a5ad69dfe83b8f878597458556f1b
YD <ydglitch@gmail.com>
2022-03-14 12:55:51 +0000

    fundraiser-contract/custodian & beneficiary

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -5,12 +5,16 @@ contract Fundraiser {
     string public url;
     string public imageURL;
     string public description;
+    address payable public beneficiary;
+    address public custodian;
     
     constructor(
         string memory _name,
         string memory _url,
         string memory _imageURL,
-        string memory _description
+        string memory _description,
+        address payable _beneficiary,
+        address _custodian
     ) 
     public
     {
@@ -18,6 +22,8 @@ contract Fundraiser {
         url = _url;
         imageURL = _imageURL;
         description = _description;
+        beneficiary = _beneficiary;
+        custodian = _custodian;
     }
 
 }
