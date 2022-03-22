dba89177bb340e662fe5fd202dfe0d5aa37dff7d
YD <ydglitch@gmail.com>
2022-03-22 14:28:15 +0000

    fundraiser-test/donation event

--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -112,6 +112,13 @@ contract("Fundraiser", accounts => {
            assert.equal(1, newDonationsCount - currentDonationsCount,
                         "donationsCount should increment by 1")
        })
+       it("emits the DonationReceived event", async () => {
+           const tx = await fundraiser.donate({from: donor, value});
+           const expectedEvent = "DonationReceived";
+           const actualEvent = tx.logs[0].event;
+
+           assert.equal(actualEvent, expectedEvent, "event should match")
+       })
    });
 
 });
