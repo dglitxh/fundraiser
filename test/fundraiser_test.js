65c68963387c9265e2528b896b4ecf5979c9781b
YD <ydglitch@gmail.com>
2022-03-22 13:17:27 +0000

    fundraiser-test/total

--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -96,7 +96,15 @@ contract("Fundraiser", accounts => {
                "values should match"
            );
            assert(dates[0], "date should be present")
-       })   
+       })  
+       it("increase the totalDonations amount", async () => {
+           const currentTotalDonations = await fundraiser.totalDonations();
+           await fundraiser.donate({from: donor,  value});
+           const newTotalDonations = await fundraiser.totalDonations();
+           const diff = newTotalDonations = currentTotalDonations;
+
+           assert.equal(diff, value, "difference should match the donation value")
+       }) 
    });
 
 });
