a002588a3be40f2a3bd9a67b2ad14d7d7ebf42a6
YD <ydglitch@gmail.com>
2022-03-23 14:04:13 +0000

    fundraiser-contract/withdraw

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -76,4 +76,9 @@ contract Fundraiser is Ownable{
         }
         return (values, dates);
     }
+
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        beneficiary.transfer(balance);
+    }
 }
