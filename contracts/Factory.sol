3b5bfba82cd44ca2e71a55d197b1dcb9f6927367
YD <ydglitch@gmail.com>
2022-03-28 12:45:54 +0000

    factory-contract

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -4,4 +4,30 @@ import "./Fundraiser.sol";
 
 contract Factory{
 
+    Fundraiser[] private _fundraisers;
+
+    function createFundraiser(
+    string memory name,
+    string memory url,
+    string memory imageURL,
+    string memory bio,
+    address payable beneficiary
+    )
+    public
+    {
+        Fundraiser fundraiser = new Fundraiser(
+        name,
+        url,
+        imageURL,
+        description,
+        beneficiary,
+        msg.sender
+    );
+    _fundraisers.push(fundraiser);
+        emit FundraiserCreated(fundraiser);
+    }
+
+    function fundraiserCount() public view returns(uint256) {
+        return _fundraisers.length
+    }
 }
