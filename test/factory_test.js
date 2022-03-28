7106c6255afe74325dc5de9597fb73d1f313bdaa
YD <ydglitch@gmail.com>
2022-03-28 12:19:38 +0000

    factory-test

--- test/factory_test.js
+++ test/factory_test.js
@@ -1,4 +1,4 @@
-const FundraiserFactoryContract = artifacts.require("FundraiserFactory");
+const FundraiserFactoryContract = artifacts.require("Factory");
 
 contract("FundraiserFactory: deployment", () => {
     it("has been deployed", async () => {
@@ -6,3 +6,34 @@ contract("FundraiserFactory: deployment", () => {
     assert(fundraiserFactory, "fundraiser factory was not deployed");
 });
 });
+
+constract("FundraiserFactory: createFundraiser", () => {
+    let fundraiserFactory;
+
+    const name = "Beneficiary Name";
+    const url = "beneficiaryname.org";
+    const url = "beneficiaryname.org";
+    const imageURL = "https://placekitten.com/600/350"
+    const bio = "Beneficiary Description"
+    const beneficiary = accounts[1];
+
+    it("increments the fundraisrCount", async () => {
+        fundraiserFactory = await FundraiserFactoryContract.deployed();
+        const currentFundraiserCount = await fundraiserFactory.fundraiserCount();
+        await fundraiserFactory.createFundraiser(
+            name,
+            url,
+            imageURL,
+            description,
+            beneficiary
+        );
+
+        const newFundraiserCount = await fundraiserFactory.fundraiserCount();
+
+        assert.equal(
+            newFundraiserCount - currentFundraiserCount,
+            1,
+            "should increment by 1"
+        )
+    })
+})
